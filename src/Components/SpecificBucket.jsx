d6f5c691d5e876710068cbe23485058b93952b03
Novodip Mondal <neptune.navo@gmail.com>
2023-09-08 14:08:57 +0600

    bug fixed

--- src/Components/SpecificBucket.jsx
+++ src/Components/SpecificBucket.jsx
@@ -16,7 +16,6 @@ import { useState } from "react";
 
 function SpecificBucket() {
   // State
-  const [isHovered, setIsHovered] = useState(false);
 
   // Query
 
@@ -34,22 +33,15 @@ function SpecificBucket() {
     return (
       <Link
         key={item.id}
-        onMouseEnter={() => {
-          dispatch(setCurrentContent(item.id));
-          setIsHovered(true);
-        }}
-        onMouseLeave={() => {
-          setIsHovered(false);
-        }}
         to={`bucket/${item.id}`}
         onClickCapture={() => handleClick(item.id)}
-        className={`shadow-md p-2 max-w-xl w-fit gap-x-4 h-fit mr-8 flex justify-between items-center px-2 drop-shadow-md text-white cursor-pointer font-semibold text-lg hover:border-2 rounded-md ${
+        className={`shadow-md p-2 max-w-xl w-fit gap-x-4 h-fit mr-8 flex justify-between items-center px-2 drop-shadow-md text-white cursor-pointer font-semibold text-lg hover:border-2 rounded-md  ${
           currentContentId === item.id && "border-2"
         } `}
         style={{ backgroundColor: item.color }}
       >
         <div className="flex flex-col transition-all duration-500">
-          {isHovered && item.id === currentContentId ? (
+          {item.id === currentContentId ? (
             <span className="text-xs font-thin flex shrink-1 mt-2 ">
               Created: {formatDate(item.created_at)}
             </span>
