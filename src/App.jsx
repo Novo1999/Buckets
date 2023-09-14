0920fa7d58815eae876fbe267567241a82d212e1
Novodip Mondal <neptune.navo@gmail.com>
2023-09-14 21:47:57 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <>
       <Toaster />
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900 overflow-x-hidden h-screen">
+      <div className=" bg-blue-900 overflow-x-hidden h-screen">
         <Home />
-        <div className="flex text-white justify-center items-center gap-2 relative lg:bottom-[1px] xl:bottom-4 bottom-4 font-thin text-md">
+        <div className="h-fit flex text-white justify-center items-center gap-2 relative lg:bottom-[1px] xl:bottom-4 bottom-4 font-thin text-md">
           Made with <AiFillHeart /> by Novo
         </div>
       </div>
